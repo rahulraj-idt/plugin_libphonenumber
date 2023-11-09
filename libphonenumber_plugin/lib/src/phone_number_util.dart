84f76e7cea7145aa73b8a422490e021b914965b8
Rahul <rahul.raj@idt.net>
2023-11-09 18:00:33 +0530

    Replace PlatformException with error

--- libphonenumber_plugin/lib/src/phone_number_util.dart
+++ libphonenumber_plugin/lib/src/phone_number_util.dart
@@ -1,5 +1,4 @@
 import 'package:libphonenumber_platform_interface/libphonenumber_platform_interface.dart';
-import 'package:flutter/services.dart';
 
 /// A wrapper class [PhoneNumberUtil] that basically switch between plugin available for `Web` or `Android or IOS` and `Other platforms` when available.
 class PhoneNumberUtil {
@@ -9,8 +8,8 @@ class PhoneNumberUtil {
   /// [isValidPhoneNumber] checks if a [phoneNumber] is valid.
   /// Accepts [phoneNumber] and [isoCode]
   /// Returns [Future<bool>].
-  static Future<bool?> isValidPhoneNumber(
-      String phoneNumber, String isoCode) async {
+  static Future<bool?> isValidPhoneNumber(String phoneNumber,
+      String isoCode) async {
     try {
       return await _platform.isValidPhoneNumber(phoneNumber, isoCode);
     } catch (_) {
@@ -21,21 +20,21 @@ class PhoneNumberUtil {
   /// [normalizePhoneNumber] normalizes a string of characters representing a phone number
   /// Accepts [phoneNumber] and [isoCode]
   /// Returns [Future<String>]
-  static Future<String?> normalizePhoneNumber(
-      String phoneNumber, String isoCode) async {
+  static Future<String?> normalizePhoneNumber(String phoneNumber,
+      String isoCode) async {
     return await _platform.normalizePhoneNumber(phoneNumber, isoCode);
   }
 
   /// [getRegionInfo] about phone number
   /// Accepts [phoneNumber] and [isoCode]
   /// Returns [Future<RegionInfo>] of all information available about the [phoneNumber]
-  static Future<RegionInfo> getRegionInfo(
-      String phoneNumber, String isoCode) async {
+  static Future<RegionInfo> getRegionInfo(String phoneNumber,
+      String isoCode) async {
     try {
       Map<String, dynamic>? response =
       await _platform.getRegionInfo(phoneNumber, isoCode);
       return RegionInfo.fromJson(response);
-    } on PlatformException catch (e) {
+    } catch (e) {
       return RegionInfo();
     }
   }
@@ -43,8 +42,8 @@ class PhoneNumberUtil {
   /// [getNumberType] get type of phone number
   /// Accepts [phoneNumber] and [isoCode]
   /// Returns [Future<PhoneNumberType>] type of phone number
-  static Future<PhoneNumberType> getNumberType(
-      String phoneNumber, String isoCode) async {
+  static Future<PhoneNumberType> getNumberType(String phoneNumber,
+      String isoCode) async {
     int? index = await _platform.getNumberType(phoneNumber, isoCode);
     return PhoneNumberType.fromIndex(index);
   }
@@ -52,11 +51,11 @@ class PhoneNumberUtil {
   /// [formatAsYouType] uses Google's libphonenumber input format as you type.
   /// Accepts [phoneNumber] and [isoCode]
   /// Returns [Future<String>]
-  static Future<String?> formatAsYouType(
-      String phoneNumber, String isoCode) async {
+  static Future<String?> formatAsYouType(String phoneNumber,
+      String isoCode) async {
     try {
       return await _platform.formatAsYouType(phoneNumber, isoCode);
-    } on PlatformException catch (e) {
+    } catch (e) {
       return null;
     }
   }
@@ -68,13 +67,13 @@ class PhoneNumberUtil {
 
   /// [getFormattedExampleNumber] Gets a valid number for the specified region, number type and number format.
   /// Accepts [isoCode], [PhoneNumberType], [PhoneNumberFormat]
-  static Future<String?> getFormattedExampleNumber(
-      String isoCode, PhoneNumberType type, PhoneNumberFormat format) async {
+  static Future<String?> getFormattedExampleNumber(String isoCode,
+      PhoneNumberType type, PhoneNumberFormat format) async {
     return await _platform.getFormattedExampleNumber(isoCode, type, format);
   }
 
-  static Future<ParsedPhoneNumber?> parsePhoneNumber(
-      String phoneNumber, String isoCode) async {
+  static Future<ParsedPhoneNumber?> parsePhoneNumber(String phoneNumber,
+      String isoCode) async {
     return await _platform.parsePhoneNumber(phoneNumber, isoCode);
   }
 }
